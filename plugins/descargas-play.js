63977e2315846e82c50408ac01bafe99fc8fedea
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-06-08 13:41:36 -0500

    Update descargas-play.js.

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -1,84 +1,48 @@
-import fetch from "node-fetch"
-import yts from 'yt-search'
-import axios from "axios"
-
-const handler = async (m, { conn, text, usedPrefix, command }) => {
-  try {
-    if (!text.trim()) {
-      return conn.reply(m.chat, `❀ Por favor, ingresa el nombre de la música a descargar.`, m)
+import yts from "yt-search";
+import { ytv, yta } from "./_ytdl.js";
+const limit = 100;
+const handler = async (m, { conn, text, command }) => {
+  if (!text) return m.reply("> Ingresa el nombre de un video o una URL de YouTube.");
+    m.react("🕛")
+    let res = await yts(text);
+    if (!res || !res.all || res.all.length === 0) {
+      return m.reply("No se encontraron resultados para tu búsqueda.");
     }
 
-    const search = await yts(text)
-    if (!search.all || search.all.length === 0) {
-      return m.reply('✧ No se encontraron resultados para tu búsqueda.')
-    }
+    let video = res.all[0];
+    let total = Number(video.duration.seconds) || 0;
 
-    const videoInfo = search.all[0]
-    if (!videoInfo) {
-      return m.reply('✧ No se pudo obtener información del video.')
-    }
+    const cap = `*「❀」${video.title}*
+> *✧ Canal : »* ${video.author.name}
+> *✧ Duración : »* ${video.duration.timestamp}
+> *✧ Vistas : »* ${video.views}
+> *✧ URL : »* ${video.url}
 
-    const { title, thumbnail, timestamp, views, ago, url, author } = videoInfo
+${wm}`;
+    await conn.sendFile(m.chat, await (await fetch(video.thumbnail)).buffer(), "image.jpg", cap, m);
 
-    if (!title || !thumbnail || !timestamp || !views || !ago || !url || !author) {
-      return m.reply('✧ Información incompleta del video.')
-    }
-
-    const vistas = formatViews(views)
-    const canal = author.name ? author.name : 'Desconocido'
-    const infoMessage = `「✦」Descargando *<${title || 'Desconocido'}>*\n\n> ✦ Canal » *${canal}*\n> ✰ Vistas » *${vistas || 'Desconocido'}*\n> ⴵ Duración » *${timestamp || 'Desconocido'}*\n> ✐ Publicación » *${ago || 'Desconocido'}*\n> 🜸 Link » ${url}`
-
-    const thumb = (await conn.getFile(thumbnail))?.data
-
-    const JT = {
-      contextInfo: {
-        externalAdReply: {
-          title: botname,
-          body: dev,
-          mediaType: 1,
-          previewType: 0,
-          mediaUrl: url,
-          sourceUrl: url,
-          thumbnail: thumb,
-          renderLargerThumbnail: true,
-        },
-      },
-    }
-
-    await conn.reply(m.chat, infoMessage, m, JT)
-
-    if (command === 'play' || command === 'playaudio' || command === 'ytmp3') {
+    if (command === "play") {
       try {
-        const api = await (await fetch(`https://api.vreden.my.id/api/ytmp3?url=${url}`)).json()
-        const resulta = api.result
-        const result = resulta.download.url
-
-        if (!result) throw new Error('⚠ El enlace de audio no se generó correctamente.')
-
-        await conn.sendMessage(m.chat, { audio: { url: result }, fileName: `${api.result.title}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m })
-      } catch (e) {
-        return conn.reply(m.chat, '⚠︎ No se pudo enviar el audio. Esto puede deberse a que el archivo es demasiado pesado o a un error en la generación de la URL. Por favor, intenta nuevamente más tarde.', m)
-      }
-    } else if (command === 'play2' || command === 'ytv' || command === 'ytmp4') {
-      try {
-        const response = await fetch(`https://api.vreden.my.id/api/ytmp4?url=${url}`)
-        const json = await response.json()
-        const resultad = json.result
-        const resultado = resultad.download.url
-
-        if (!resultad || !resultado) throw new Error('⚠ El enlace de video no se generó correctamente.')
-
-        await conn.sendMessage(m.chat, { video: { url: resultado }, fileName: resultad.title, mimetype: 'video/mp4', caption: title }, { quoted: m })
-      } catch (e) {
-        return conn.reply(m.chat, '⚠︎ No se pudo enviar el video. Esto puede deberse a que el archivo es demasiado pesado o a un error en la generación de la URL. Por favor, intenta nuevamente más tarde.', m)
-      }
-    } else {
-      return conn.reply(m.chat, '✧︎ Comando no reconocido.', m)
+    const api = await yta(video.url)
+ await conn.sendFile(m.chat, api.result.download, api.result.title, "", m);
+            await m.react("✅");
+        } catch (error) {
+          return error.message
+        }
+    } else if (command === "play2" || command === "playvid") {
+    try {
+      const api = await ytv(video.url)
+      const res = await fetch(api.url);
+      const cont = res.headers.get('Content-Length');
+      const bytes = parseInt(cont, 10);
+      const sizemb = bytes / (1024 * 1024);
+      const doc = sizemb >= limit;
+ await conn.sendFile(m.chat, api.url, api.title, "", m, null, { asDocument: doc, mimetype: "video/mp4" });
+            await m.react("✅");
+        } catch (error) {
+          return error.message
+        }
     }
-
-  } catch (error) {
-    return m.reply(`⚠︎ Ocurrió un error: ${error}`)
-  }
 }
 
 handler.command = handler.help = ['play', 'playaudio', 'ytmp3', 'play2', 'ytv', 'ytmp4']
@@ -86,18 +50,3 @@ handler.tags = ['descargas']
 handler.group = true
 
 export default handler
-
-function formatViews(views) {
-  if (views === undefined) {
-    return "No disponible"
-  }
-
-  if (views >= 1_000_000_000) {
-    return `${(views / 1_000_000_000).toFixed(1)}B (${views.toLocaleString()})`
-  } else if (views >= 1_000_000) {
-    return `${(views / 1_000_000).toFixed(1)}M (${views.toLocaleString()})`
-  } else if (views >= 1_000) {
-    return `${(views / 1_000).toFixed(1)}k (${views.toLocaleString()})`
-  }
-  return views.toString()
-}
